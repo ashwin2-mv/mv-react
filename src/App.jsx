fba05084d4df7a986da3f9637c77472c20c88a76
ashwin2-mv <ashwinmarangad@gmail.com>
2025-05-29 13:24:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div class="fblogin" style={{ backgroundColor: "F5F5F5" }}>
+      
+        
+      <img src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg"width={400} alt="" />
+      <h2 style={{
+        marginBottom:'12px'
+      }}>
+        Facebook helps you connect and<br></br>share with the people in your
+        life.
+      </h2>
+      <div class="login-fb card">
+        <h3>Log in to Facebook</h3>
+        <div>
+          <label htmlFor="fname">User name:</label>
+          <input
+            type="text"
+            id="fname"
+            name="fname"
+            placeholder="Email adress or phone number"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="lname">Password:</label>
+
+          <input
+            type="password"
+            id="lname"
+            name="lname"
+            placeholder="password"
+          />
+        </div>
+
+        <br />
+        <input class="button" type="submit" value="Log in" />
+        <a href="#">forgotten button?</a>
+
+        <input class="createbutton" type="submit" value="Create new account" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
